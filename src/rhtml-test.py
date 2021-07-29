d5024cfbb112b31f55d18dbd6dbd0f1a41ed7cb7
fouadsan <f.benayad95@gmail.com>
2021-07-29 16:35:11 +0100

    Scraped web page from the internet: my portfolio

--- src/rhtml-test.py
+++ src/rhtml-test.py
@@ -1,15 +1,26 @@
-from requests_html import HTML
+from requests_html import HTML, HTMLSession
+import csv
 
-with open('test.html') as html_file:
-    source = html_file.read()
-    html = HTML(html=source)
+with open('scraped.csv', 'w') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['title', 'description', 'image'])
 
-articles = html.find('div.article')
+    session = HTMLSession()
+    response = session.get('https://fouadben.herokuapp.com/')
 
-for article in articles:
-    title = article.find('h2', first=True).text
-    description = article.find('p', first=True).text
+    services = response.html.find('div.service-item')
 
-    print(title)
-    print(description)
-    print()
+    for service in services:
+
+        title = service.find('h4', first=True).text
+
+        description = service.find('p', first=True).text
+
+        image_src = service.find('img', first=True).attrs['src']
+
+        print(title)
+        print(description)
+        print(image_src)
+        print()
+
+        csv_writer.writerow([title, description, image_src])
